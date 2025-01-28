f950515d067646a7accc8d7264eebdcbd0590bb1
brein594 <brein594@gmail.com>
2025-01-28 21:50:23 +0300

    completed step 1, 2

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+application {
+    mainClass = "hexlet.code.App"
+}
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
+
