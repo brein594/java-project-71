4f464a9052369dde81c5bd9ac6d0d490ecabf21f
brein594 <brein594@gmail.com>
2025-02-09 12:25:41 +0300

    step 11 completed

--- app/src/main/java/hexlet/code/formatters/JsonFormatters.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatters.java
@@ -4,48 +4,68 @@ import java.util.List;
 import java.util.Map;
 
 public class JsonFormatters {
+    private static String complexValue(Object value) {
+        if (value.equals("null")) {
+            return "null";
+        }
+        if (value instanceof List || value instanceof Map) {
+            return "[complex value]";
+        }
+        if (value instanceof String) {
+            return "\"" + value + "\"";
+        }
+        return value.toString();
+    }
 
     public static String getAnswer(List<String> unionListKey, Map<String, Object> data1, Map<String, Object> data2) {
-        var result = new StringBuilder("{\n");
+        var result = new StringBuilder("{");
         for (var key : unionListKey) {
             if (data1.getOrDefault(key, null) != null) {
                 if (data2.getOrDefault(key, null) != null) {
-                    if (data1.get(key).equals(data2.get(key))) {
-                        result.append("    ");
-                        result.append(key);
-                        result.append(": ");
-                        result.append(data1.get(key));
+                    if (!data1.get(key).equals(data2.get(key))) {
                         result.append(System.lineSeparator());
-                    } else {
-                        result.append("  - ");
+                        result.append("\"");
                         result.append(key);
-                        result.append(": ");
-                        result.append(data1.get(key));
+                        result.append("\": ");
+                        result.append(complexValue(data1.get(key)));
+                        result.append(",");
                         result.append(System.lineSeparator());
-                        result.append("  + ");
+                        result.append("\"");
                         result.append(key);
-                        result.append(": ");
-                        result.append(data2.get(key));
-                        result.append(System.lineSeparator());
+                        result.append("\": ");
+                        result.append(complexValue(data2.get(key)));
+                        result.append(",");
                     }
                 } else {
-                    result.append("  - ");
-                    result.append(key);
-                    result.append(": ");
-                    result.append(data1.get(key));
                     result.append(System.lineSeparator());
+                    result.append("\"");
+                    result.append(key);
+                    result.append("\": ");
+                    result.append(complexValue(data1.get(key)));
+                    result.append(",");
+                    result.append(System.lineSeparator());
+                    result.append("\"");
+                    result.append(key);
+                    result.append("\": ");
+                    result.append("\"not_value\",");
                 }
             } else if (data2.getOrDefault(key, null) != null) {
-                result.append("  + ");
-                result.append(key);
-                result.append(": ");
-                result.append(data2.get(key));
                 result.append(System.lineSeparator());
+                result.append("\"");
+                result.append(key);
+                result.append("\": ");
+                result.append("\"not_value\",");
+                result.append(System.lineSeparator());
+                result.append("\"");
+                result.append(key);
+                result.append("\": ");
+                result.append(complexValue(data2.get(key)));
+                result.append(",");
             } else {
                 result.append(System.lineSeparator());
             }
         }
-        result.append("}");
-        return result.toString();
+        var resultEnd = result.toString();
+        return resultEnd.substring(0, resultEnd.length() - 2) + "\n}";
     }
 }
