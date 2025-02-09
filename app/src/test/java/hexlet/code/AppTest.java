50324a1b726a8a389954ffc7173eea2d95fdea2e
brein594 <brein594@gmail.com>
2025-02-09 12:40:30 +0300

    add test JSON format

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -141,38 +141,40 @@ class AppTest {
     }
 
     @Test
-    public void generateJsonStep9Test() throws Exception {
+    public void generateJsonStep11Test() throws Exception {
         String expected =
                 "{\n"
-                        + "    chars1: [a, b, c]\n"
-                        + "  - chars2: [d, e, f]\n"
-                        + "  + chars2: false\n"
-                        + "  - checked: false\n"
-                        + "  + checked: true\n"
-                        + "  - default: null\n"
-                        + "  + default: [value1, value2]\n"
-                        + "  - id: 45\n"
-                        + "  + id: null\n"
-                        + "  - key1: value1\n"
-                        + "  + key2: value2\n"
-                        + "    numbers1: [1, 2, 3, 4]\n"
-                        + "  - numbers2: [2, 3, 4, 5]\n"
-                        + "  + numbers2: [22, 33, 44, 55]\n"
-                        + "  - numbers3: [3, 4, 5]\n"
-                        + "  + numbers4: [4, 5, 6]\n"
-                        + "  + obj1: {nestedKey=value, isNested=true}\n"
-                        + "  - setting1: Some value\n"
-                        + "  + setting1: Another value\n"
-                        + "  - setting2: 200\n"
-                        + "  + setting2: 300\n"
-                        + "  - setting3: true\n"
-                        + "  + setting3: none\n"
-                        + "}";
+                       + "\"chars2\": [complex value],\n"
+                       + "\"chars2\": false,\n"
+                       + "\"checked\": false,\n"
+                       + "\"checked\": true,\n"
+                       + "\"default\": null,\n"
+                       + "\"default\": [complex value],\n"
+                       + "\"id\": 45,\n"
+                       + "\"id\": null,\n"
+                       + "\"key1\": \"value1\",\n"
+                       + "\"key1\": \"not_value\",\n"
+                       + "\"key2\": \"not_value\",\n"
+                       + "\"key2\": \"value2\",\n"
+                       + "\"numbers2\": [complex value],\n"
+                       + "\"numbers2\": [complex value],\n"
+                       + "\"numbers3\": [complex value],\n"
+                       + "\"numbers3\": \"not_value\",\n"
+                       + "\"numbers4\": \"not_value\",\n"
+                       + "\"numbers4\": [complex value],\n"
+                       + "\"obj1\": \"not_value\",\n"
+                       + "\"obj1\": [complex value],\n"
+                       + "\"setting1\": \"Some value\",\n"
+                       + "\"setting1\": \"Another value\",\n"
+                       + "\"setting2\": 200,\n"
+                       + "\"setting2\": 300,\n"
+                       + "\"setting3\": true,\n"
+                       + "\"setting3\": \"none\n"
+                       + "}";
         var path1 = "/home/hexlet/play_brain/java-project-71/app/src/test/resources/file1s9.json";
         var path2 = "/home/hexlet/play_brain/java-project-71/app/src/test/resources/file2s9.json";
         String actual = Differ.generate(path1, path2, "json");
         assertEquals(expected, actual, "errorTest");
     }
-
 
 }
